8027ccffb8936c703f95899e6e8da3303253ce7f
Shogo Yamashita <s24c1130pf@s.chibakoudai.jp>
2025-12-04 12:01:19 +0900

    サービスを実装

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -1,11 +1,30 @@
 import rclpy
 from rclpy.node import Node
-from person_msgs.msg import Person
-
-def cb(msg):
-    node.get_logger().info("Listen: %s" % msg)
+from person_msgs.srv import Query
 
 rclpy.init()
 node = Node("listener")
-sub = node.create_subscription(Person, "person", cb, 10)
-rclpy.spin(node)
+
+def main():
+    client = node.create_client(Query, 'query') #サービスのクライアントの作成
+    while not client.wait_for_service(timeout_sec=1.0): #サービスの待ち待ち
+        node.get_logger().info('待機中')
+
+    req = Query.Request()
+    req.name = "山下正悟"
+    future = client.call_async(req) #非同期でサービスを呼び出し
+
+    while rclpy.ok(): #サービスが起動したら開始
+        rclpy.spin_once(node) #一回だけサービスを呼び出したら終わり
+        if future.done():     #終わっていたら
+            try:
+                response = future.result() #結果を受取り
+            except:
+                node.get_logger().info('呼び出し失敗')
+            else: #このelseは「exceptじゃなかったら」という意味のelse
+                node.get_logger().info("age: {}".format(response.age))
+
+            break
+
+    node.destroy_node() #ノードの後始末   （後始末は無限ループでないので書きました。
+    rclpy.shutdown()    #通信の後始       ただ、本来は無限ループでも書いたほうがよいです。）
